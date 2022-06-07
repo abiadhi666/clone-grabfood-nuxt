51603e4ccd8642cd5f1bd8b075e2def16b1e8749
abiadhi666 <https://abiadhi666:abiadhi12345@github.com>
2022-06-07 10:46:54 +0700

    start config

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img src="../static/assets/img/logo-grabfood2.svg" alt="logo-grabfood" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
