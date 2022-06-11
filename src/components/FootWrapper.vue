754cc10de2bee2a53075eacd8d556e5adb0fe7da
abiadhi666 <https://abiadhi666:abiadhi12345@github.com>
2022-06-11 12:30:10 +0700

    update footwrapper and footerwrapper.

--- src/components/FootWrapper.vue
+++ src/components/FootWrapper.vue
@@ -1,6 +1,138 @@
 <template>
   <div class="foot-wrapper">
-    <v-container>
+    <v-container class="mobile">
+      <div class="foot-logo">
+        <v-img
+          class="img-foot"
+          src="https://food.grab.com/static/images/logo-grabfood-mono.svg"
+          contain
+        ></v-img>
+        <v-divider class="mt-6"></v-divider>
+      </div>
+      <v-expansion-panels accordion flat>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="foot-header">Frequently Searched</div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ul class="foot-list">
+              <li>
+                <a href="#"> Burger King Menu </a>
+              </li>
+              <li>
+                <a href="#">Chatime Menu</a>
+              </li>
+              <li>
+                <a href="#">Domino's Pizza Menu</a>
+              </li>
+              <li>
+                <a href="#">Hokben Menu</a>
+              </li>
+              <li>
+                <a href="#">JCO Delivery</a>
+              </li>
+              <li>
+                <a href="#">KFC Menu</a>
+              </li>
+              <li>
+                <a href="#">McDonalds Menu</a>
+              </li>
+              <li>
+                <a href="#">Pizza Hut Delivery</a>
+              </li>
+              <li>
+                <a href="#">Richeese Menu</a>
+              </li>
+              <li>
+                <a href="#">Menu Solaria</a>
+              </li>
+              <li>
+                <a href="#">Starbucks Menu</a>
+              </li>
+              <li>
+                <a href="#">Yoshinoya Menu</a>
+              </li>
+            </ul>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="foot-header">Popular Cuisines</div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ul class="foot-list">
+              <li>
+                <a href="#">Chinese Food</a>
+              </li>
+              <li>
+                <a href="#">Fast Food</a>
+              </li>
+              <li>
+                <a href="#">Indian Food</a>
+              </li>
+              <li>
+                <a href="#">Indonesian Food</a>
+              </li>
+              <li>
+                <a href="#">Italian Food</a>
+              </li>
+              <li>
+                <a href="#">Japanese Food</a>
+              </li>
+              <li>
+                <a href="#">Korean Food</a>
+              </li>
+              <li>
+                <a href="#">Thai Food</a>
+              </li>
+              <li>
+                <a href="#">Vietnamese Food</a>
+              </li>
+            </ul>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="foot-header">About Grab</div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ul class="foot-list">
+              <li>
+                <a href="#">About Grab</a>
+              </li>
+              <li>
+                <a href="#">About GrabFood</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+            </ul>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="foot-header">Support</div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <ul class="foot-list">
+              <li>
+                <a href="#">Help</a>
+              </li>
+              <li>
+                <a href="#">FAQs</a>
+              </li>
+              <li>
+                <a href="#">Be a GrabFood Merchant</a>
+              </li>
+              <li>
+                <a href="#">Drive With Grab</a>
+              </li>
+            </ul>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
+    <v-container class="desktop">
       <div class="foot-logo">
         <v-img
           class="img-foot"
